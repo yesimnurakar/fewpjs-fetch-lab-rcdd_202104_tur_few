6b4de17fe77c3d7f37af28b8372c01463b100823
Yesim nur Akar <yesimnurakar@gmail.com>
2021-05-02 21:15:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 function fetchBooks(){
   fetch('https://anapioficeandfire.com/api/books')
+  return fetch('https://anapioficeandfire.com/api/books')
   .then(function(response) {
-    console.log(renderBooks);
+    //console.log(renderBooks);
     return response.json();
   })
   .then(function(json) {
-    console.log(renderBooks);
-    return json
-  })
+    console.log(renderBooks(json));
+
+  });
+
   // To pass the tests, don't forget to return your fetch!
 .then(console.log)
 }
